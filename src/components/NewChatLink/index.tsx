273ba449ebc2c7af3e369e26c916beeed98933ed
이명재 <arkk20000@gmail.com>
2023-06-01 14:48:35 +0900

    FIX :: New Chat 아이콘, AddIcon.tsx으로 수정

--- src/components/NewChatLink/index.tsx
+++ src/components/NewChatLink/index.tsx
@@ -1,22 +1,10 @@
+import AddIcon from "../../assets/icons/AddIcon";
 import { NewChatLinkLayout } from "./style";
 
 export default function NewChatLink() {
   return (
     <NewChatLinkLayout>
-      <svg
-        stroke="currentColor"
-        fill="none"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        height="1.2em"
-        width="1.2em"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <line x1="12" y1="5" x2="12" y2="19"></line>
-        <line x1="5" y1="12" x2="19" y2="12"></line>
-      </svg>
+      <AddIcon />
       New chat
     </NewChatLinkLayout>
   );
