c43c1bdb26f9640de7b166a8152dfdc9f79f0aa6
이명재 <arkk20000@gmail.com>
2023-06-24 02:57:41 +0900

    REFACTOR :: GPTField 컴포넌트 리펙토링

--- src/components/common/GPTField/index.tsx
+++ src/components/common/GPTField/index.tsx
@@ -1,36 +1,37 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import GPTCopyright from "./GPTCopyright";
 import * as S from "./style";
 import SendIcon from "@/assets/icons/SendIcon";
+import { useRecoilState } from "recoil";
+import { valueState } from "@/recoil/gptField/atom";
+import { handleTextareaHeight } from "@/utils/handleTextareaHeight";
 
-export default function GPTField() {
-  const [message, setMessage] = useState("");
+interface PropTypes {
+  handleSubmit: React.FormEventHandler<HTMLFormElement>;
+}
 
+export default function GPTField({ handleSubmit }: PropTypes) {
+  const [value, setValue] = useRecoilState(valueState);
+
+  const formRef = useRef<HTMLFormElement>(null);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const handleTextareaHeight = (
-    event: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setMessage(event.target.value);
-    const textarea = textareaRef.current;
-
-    if (textarea) {
-      textarea.style.height = "auto";
-      textarea.style.height = textarea.scrollHeight + "px";
-      textarea.scrollTop = textarea.scrollHeight;
-    }
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
+    handleTextareaHeight(textareaRef.current);
   };
 
   return (
     <S.GPTBottomBox>
-      <S.Form>
+      <S.Form ref={formRef} onSubmit={handleSubmit}>
         <S.Textarea
           ref={textareaRef}
           rows={1}
-          onChange={handleTextareaHeight}
+          onChange={onChange}
           placeholder="Send a message."
+          value={value}
         />
-        <S.SendButton disabled={!message}>
+        <S.SendButton disabled={!value}>
           <SendIcon />
         </S.SendButton>
       </S.Form>
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>
 );
