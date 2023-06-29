b83235191f99ed719e84a20750e6dfe4bd071997
NameIsUser06 <woosoungcho@gmail.com>
2023-06-29 12:10:01 +0900

    ADD : 유저 별 히스토리 확인

--- src/components/common/GPTHistoryBar/index.tsx
+++ src/components/common/GPTHistoryBar/index.tsx
@@ -8,13 +8,13 @@ import { useSetRecoilState } from "recoil";
 import SideBarIcon from "@/assets/icons/SideBarIcon";
 import { visibleSideBarState } from "@/recoil/sideBar.atom";
 import { useEffect, useState } from "react";
-import { db } from "@/services/firebase";
+import { auth, db } from "@/services/firebase";
 import { collection, onSnapshot } from "firebase/firestore";
 
 export default function GPTHistoryBar() {
   const location = useLocation();
   // const historyItems = useRecoilValue(History);
-  const [histories, setHistories] = useState<{ id: string; title: string }[]>(
+  const [histories, setHistories] = useState<{ id: string; title: string; uid: string}[]>(
     []
   );
   const setVisibleSideBar = useSetRecoilState(visibleSideBarState);
@@ -22,7 +22,7 @@ export default function GPTHistoryBar() {
   useEffect(() => {
     const unsub = onSnapshot(collection(db, "history"), (snapshot) => {
       setHistories(
-        snapshot.docs.map((doc) => doc.data() as { id: string; title: string })
+        snapshot.docs.map((doc) => doc.data() as { id: string; title: string; uid: string})
       );
     });
 
@@ -39,12 +39,14 @@ export default function GPTHistoryBar() {
       </S.TopButtons>
       <Column style={{ flex: 1 }}>
         {histories.map((item) => (
+          item.uid === auth.currentUser?.uid ? 
           <HistoryLink
             key={item.id}
             id={item.id}
             title={item.title}
             isCurrentPage={location.pathname.split("/")[2] === item.id}
           />
+          : null
         ))}
       </Column>
       <Features />
