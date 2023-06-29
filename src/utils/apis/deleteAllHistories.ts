3165e2dd7776d662affb5404f7ea4ef61667ca5d
이명재 <arkk20000@gmail.com>
2023-06-29 13:19:40 +0900

    FIX : Clear conversations 다 지워지는 버그 해결

--- src/utils/apis/deleteAllHistories.ts
+++ src/utils/apis/deleteAllHistories.ts
@@ -1,11 +1,23 @@
-import { db } from "@/services/firebase";
-import { deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "@/services/firebase";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 
 export default async function deleteAllHistories(historyIds: string[]) {
   console.log(historyIds);
-  historyIds.forEach(async (id) => {
-    await deleteDoc(doc(db, "history", id));
-    await deleteDoc(doc(db, "chat", id));
+  const historiesRef = collection(db, "history");
+  const q = query(historiesRef, where("uid", "==", auth.currentUser?.uid));
+
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach(async (d) => {
+    await deleteDoc(doc(db, "history", d.data().id));
+    await deleteDoc(doc(db, "chat", d.data().id));
   });
 
   return true;
